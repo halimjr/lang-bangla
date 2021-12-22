3cc4e2a4fdc27232a673e01c0c03f66452ee69ff
Abdul Halim Jr <ihalimjr@gmail.com>
2021-12-22 15:08:54 +0600

    Update config.js

--- locale/config.js
+++ locale/config.js
@@ -1,3 +1,27 @@
+var numbers = {
+  0:'০',
+  1:'১',
+  2:'২',
+  3:'৩',
+  4:'৪',
+  5:'৫',
+  6:'৬',
+  7:'৭',
+  8: '৮',
+  9:'৯'
+};
+
+function replaceNumbers(input) {
+  var output = [];
+  for (var i = 0; i < input.length; ++i) {
+    if (numbers.hasOwnProperty(input[i])) {
+      output.push(numbers[input[i]]);
+    } else {
+      output.push(input[i]);
+    }
+  }
+  return output.join('');
+}
 
 const texts = {
   s: 'কয়েক সেকেন্ড',
@@ -7,10 +31,10 @@ const texts = {
   hh: '%d ঘন্টা',
   d: ['এক দিন', 'এক দিন'],
   dd: ['%d দিন', '%d দিন'],
-  M: ['ein Monat', 'einem Monat'],
-  MM: ['%d Monate', '%d Monaten'],
-  y: ['ein Jahr', 'einem Jahr'],
-  yy: ['%d Jahre', '%d Jahren']
+  M: ['এক মাস', 'এক মাস'],
+  MM: ['%d মাস', '%d মাস'],
+  y: ['এক বছর', 'এক বছর'],
+  yy: ['%d বছর', '%d বছর']
 }
 
 function relativeTimeFormatter(number, withoutSuffix, key) {
@@ -18,30 +42,30 @@ function relativeTimeFormatter(number, withoutSuffix, key) {
   if (Array.isArray(l)) {
     l = l[withoutSuffix ? 0 : 1]
   }
-  return l.replace('%d', number)
+  return replaceNumbers(l.replace('%d', number))
 }
 
 dayjs.locale({
-  name: 'de',
-  weekdays: 'Sonntag_Montag_Dienstag_Mittwoch_Donnerstag_Freitag_Samstag'.split('_'),
-  weekdaysShort: 'So._Mo._Di._Mi._Do._Fr._Sa.'.split('_'),
-  weekdaysMin: 'So_Mo_Di_Mi_Do_Fr_Sa'.split('_'),
-  months: 'Januar_Februar_März_April_Mai_Juni_Juli_August_September_Oktober_November_Dezember'.split('_'),
-  monthsShort: 'Jan_Feb_März_Apr_Mai_Juni_Juli_Aug_Sept_Okt_Nov_Dez'.split('_'),
+  name: 'en',
+  weekdays: 'রবিবার_সোমবার_মঙ্গলবার_বুধবার_বৃহস্পতিবার_শুক্রবার_শনিবার'.split('_'),
+  weekdaysShort: 'রবি._সোম._মঙ্গল._বুধ._বৃহস্পতি._শুক্র._শনি.'.split('_'),
+  weekdaysMin: 'রবি_সোম_মঙ্গল_বুধ_বৃহস্পতি_শুক্র_শনি'.split('_'),
+  months: 'জানুয়ারি_ফেব্রুয়ারি_মার্চ_এপ্রিল_মে_জুন_জুলাই_আগস্ট_সেপ্টেম্বর_অক্টোবর_নভেম্বর_ডিসেম্বর'.split('_'),
+  monthsShort: 'জানু_ফেব্রু_মার্চ_এপ্রিল_মে_জুন_জুলাই_আগস্ট_সেপ্ট_অক্টো_নভে_ডিসে'.split('_'),
   ordinal: n => `${n}.`,
-  weekStart: 1,
+  weekStart: 5,
   yearStart: 4,
   formats: {
-    LTS: 'HH:mm:ss',
-    LT: 'HH:mm',
+    LTS: 'hh:mm:ss A',
+    LT: 'hh:mm A',
     L: 'DD.MM.YYYY',
     LL: 'D. MMMM YYYY',
-    LLL: 'D. MMMM YYYY HH:mm',
-    LLLL: 'dddd, D. MMMM YYYY HH:mm'
+    LLL: 'D. MMMM YYYY hh:mm A',
+    LLLL: 'dddd, D MMMM, YYYY hh:mm A'
   },
   relativeTime: {
-    future: 'in %s',
-    past: 'vor %s',
+    future: '%s ভিতরে',
+    past: '%s আগে',
     s: relativeTimeFormatter,
     m: relativeTimeFormatter,
     mm: relativeTimeFormatter,
